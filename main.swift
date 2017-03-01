3cf9922ae06c526bb6b4bf5a5f8ef82a44e4f805
Linker <447119040@qq.com>
2017-03-01 11:37:21 +0800

    0301-更改后
    用索引enumerated输出

--- main.swift
+++ main.swift
@@ -12,7 +12,7 @@ var sort:[Int] = [13,56,99,9,48,36,24,72,81,4]
 print("排序前：\(sort)")
 for i in 0..<sort.count
 {
-    for j in 0..<sort.count - (i+1)
+    for j in 0..<sort.count - i - 1
     {
        if sort[j]>sort[j+1]
        {
@@ -22,9 +22,11 @@ for i in 0..<sort.count
         }
     }
 }
-print("更改后")
+for (index , value) in sort.enumerated()
+{
+    print("更改后第\(index+1)个元素是 \(value)")
+}
 for i in 0..<sort.count
 {
     print(sort[i])
 }
-
